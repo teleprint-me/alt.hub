5ad14e5772225c89c5f5bdcd892948091f7e5fa7
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-05 00:53:23 -0400

    feat: Add ported custom CLI tool for huggingface hub

---
+++ hf/cli/hub.py
@@ -0,0 +1,103 @@
+"""
+Module: hf.cli.hub
+
+NOTE: This is still a Work in Progress.
+"""
+
+import click
+
+from hf.download import HuggingFaceDownload
+from hf.env import get_environment
+from hf.upload import HuggingFaceUpload
+
+
+def common_options(command_func):
+    decorators = [
+        click.option(
+            "-i",
+            "--repo_id",
+            prompt="Hugging Face repository ID",
+            help="The Hugging Face repository ID.",
+        ),
+        click.option(
+            "-t",
+            "--repo_type",
+            help="The type of repository. `model`, `dataset`, or `space`. Defaults to `None`.",
+        ),
+        click.option(
+            "-e",
+            "--env_path",
+            help="The .env file path.",
+        ),
+        click.option(
+            "-a",
+            "--api_token",
+            help="Hugging Face API token. Overrides configuration file.",
+        ),
+    ]
+    for decorator in reversed(decorators):
+        command_func = decorator(command_func)
+    return command_func
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name="upload")
+@click.option(
+    "-p",
+    "--local_path",
+    prompt="Local path to upload",
+    help="The local path from which to upload files or directories.",
+)
+@common_options
+def upload_command(repo_id, repo_type, local_path, env_path, api_token):
+    if not api_token and env_path:
+        api_token = get_environment(
+            file_path=env_path, variable="HUGGINGFACE_WRITE_API"
+        )
+
+    huggingface = HuggingFaceUpload(api_token)
+    huggingface.upload(local_path, repo_id, repo_type)
+
+
+@cli.command(name="download")
+@click.option(
+    "-p",
+    "--local_path",
+    prompt="Local path to download",
+    help="The local path from which to download files or directories.",
+)
+@click.option(
+    "-f",
+    "--is_file",
+    is_flag=True,
+    help="Specify this flag if the local path is a file. If not set, the path is treated as a directory.",
+)
+@click.option(
+    "-d",
+    "--resume",
+    is_flag=True,
+    help="Specify this flag to continue downloading partially downloaded files. If not set, the download will start over.",
+)
+@common_options
+def download_command(
+    repo_id,
+    repo_type,
+    local_path,
+    resume,
+    is_file,
+    env_path,
+    api_token,
+):
+    if not api_token and env_path:
+        api_token = get_environment(file_path=env_path, variable="HUGGINGFACE_READ_API")
+
+    huggingface = HuggingFaceDownload(api_token)
+    huggingface.download(local_path, repo_id, repo_type, resume, is_file)
+
+
+if __name__ == "__main__":
+    cli()
